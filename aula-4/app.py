15645903fdc7822855bb2ddf77bb325b33dc23c4
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-10 12:42:25 -0300

    Feat: métricas criadas

--- aula-4/app.py
+++ aula-4/app.py
@@ -40,3 +40,27 @@ df_filtrado= df[
     (df["employment_type"].isin(contratos_selecionados)) &
     (df["company_size"].isin(tamanhos_selecionados))
 ]
+
+# --- Conteúdo Principal ---
+st.title("🎲 Dashboard de Análise de Salários na Área de Dados")
+st.markdown("Explore os dados salariais na área de dados nos últimos anos. Utilize os filtros à esquerda para refinar sua análise.")
+
+# --- Métricas Principais (KPIs) ---
+st.subheader("Métricas gerais (Salário anual em USD)")
+
+if not df_filtrado.empty:
+    salario_medio = df_filtrado['salary_in_usd'].mean()
+    salario_maximo = df_filtrado['salary_in_usd'].max()
+    total_registros = df_filtrado.shape[0]
+    cargo_mais_frequente = df_filtrado["job_title"].mode()[0]
+else:
+    salario_medio, salario_mediano, salario_maximo, total_registros, cargo_mais_comum = 0, 0, 0, ""
+
+#divisão de colunas
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Salário médio", f"${salario_medio:,.0f}")
+col2.metric("Salário máximo", f"${salario_maximo:,.0f}")
+col3.metric("Total de registros", f"{total_registros:,}")
+col4.metric("Cargo mais frequente", cargo_mais_frequente)
+
+st.markdown("---")
